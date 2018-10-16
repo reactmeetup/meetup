494ecd23b5f737a6c73aafc1679165f1bfd64d94
Vojtech Toth <vojtech.toth@globallogic.com>
2018-10-16 18:29:03 +0200

    test

--- src/Home.js
+++ src/Home.js
@@ -6,25 +6,25 @@ import { Actions } from 'react-native-router-flux';
 import List from './reusable/List';
 
 class Home extends React.Component {
-	render() {
-		return (
-			<Container>
-				<Content>
-					<Card>
-						<CardItem>
-							<Body style={styles.container}>
-								<Text>Home screen</Text>
-								<Button onPress={() => Actions.gifs()} style={styles.btn}>
-									<Text style={styles.txt}>Go to gifs</Text>
-								</Button>
-							</Body>
-						</CardItem>
-					</Card>
-				</Content>
-				<List data={this.props.favorites} numColumns={1} />
-			</Container>
-		);
-	}
+    render() {
+        return (
+            <Container>
+                <Content>
+                    <Card>
+                        <CardItem>
+                            <Body style={styles.container}>
+                                <Text>Home screenfdsfd</Text>
+                                <Button onPress={() => Actions.gifs()} style={styles.btn}>
+                                    <Text style={styles.txt}>Go to gifs</Text>
+                                </Button>
+                            </Body>
+                        </CardItem>
+                    </Card>
+                </Content>
+                <List data={this.props.favorites} numColumns={1} />
+            </Container>
+        );
+    }
 }
 
 const mapStateToProps = ({ favorites }) => ({ favorites });
@@ -32,18 +32,18 @@ const mapStateToProps = ({ favorites }) => ({ favorites });
 export default connect(mapStateToProps)(Home);
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	btn: {
-		alignSelf: 'center',
-		margin: 5
-	},
-	txt: {
-		padding: 5,
-		color: 'white'
-	}
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    btn: {
+        alignSelf: 'center',
+        margin: 5
+    },
+    txt: {
+        padding: 5,
+        color: 'white'
+    }
 });
